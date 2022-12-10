d5cd02d7b96f7edd97c8d98f1b4e97dfa720a126
Magdahime <magdalena.majkowska1@gmail.com>
2022-12-10 10:48:26 +0100

    Day8 part2

--- src/Puzzles/puzzle8.cpp
+++ src/Puzzles/puzzle8.cpp
@@ -2,6 +2,7 @@
 #include "Puzzles.hpp"
 #include <algorithm>
 #include <deque>
+#include <iterator>
 #include <set>
 #include <vector>
 
@@ -9,7 +10,7 @@ namespace AdventOfCode {
 
 namespace Puzzles {
 
-struct MaxHeightInEachDirection {
+struct MaxValueInEachDirection {
   int up = 0;
   int down = 0;
   int left = 0;
@@ -34,10 +35,10 @@ getNthColumn(const std::vector<std::vector<int>> &referenceVector, int column) {
   return columnVector;
 }
 
-std::vector<std::vector<MaxHeightInEachDirection>>
+std::vector<std::vector<MaxValueInEachDirection>>
 createMaxHeightMap(const std::vector<std::vector<int>> &treeMap) {
 
-  std::vector<std::vector<MaxHeightInEachDirection>> heightMap;
+  std::vector<std::vector<MaxValueInEachDirection>> heightMap;
   heightMap.resize(treeMap.size());
   for (auto &row : heightMap) {
     row.resize(treeMap[0].size());
@@ -47,7 +48,7 @@ createMaxHeightMap(const std::vector<std::vector<int>> &treeMap) {
     for (int column = 0; column < treeMap[0].size(); column++) {
 
       auto columnVector = getNthColumn(treeMap, column);
-      auto maxHeight = MaxHeightInEachDirection{
+      auto maxHeight = MaxValueInEachDirection{
           .up = row == 0 ? -1
                          : *std::max_element(columnVector.begin(),
                                              columnVector.begin() + row),
@@ -70,6 +71,75 @@ createMaxHeightMap(const std::vector<std::vector<int>> &treeMap) {
   return heightMap;
 }
 
+std::vector<std::vector<int>>
+createScenicScoreMap(const std::vector<std::vector<int>> &treeMap) {
+
+  std::vector<std::vector<int>> scenicScoreMap;
+  scenicScoreMap.resize(treeMap.size());
+  for (auto &row : scenicScoreMap) {
+    row.resize(treeMap[0].size());
+  }
+
+  for (int row = 0; row < treeMap.size(); row++) {
+    for (int column = 0; column < treeMap[0].size(); column++) {
+
+      auto columnVector = getNthColumn(treeMap, column);
+
+      const auto currentHeight = treeMap[row][column];
+
+      auto scenicScore = MaxValueInEachDirection{
+          .up = static_cast<int>(
+              row == 0 ? 0
+                       : std::abs(std::distance(
+                             columnVector.rend() - row - 1,
+                             std::find_if(columnVector.rbegin() +
+                                              (columnVector.size() - row),
+                                          columnVector.rend() - 1,
+                                          [&](int treeHeight) {
+                                            return treeHeight >= currentHeight;
+                                          })))),
+          .down = static_cast<int>(
+              row == treeMap.size() - 1
+                  ? 0
+                  : std::abs(std::distance(
+                        columnVector.begin() + row,
+                        std::find_if(columnVector.begin() + row + 1,
+                                     columnVector.end() - 1,
+                                     [&](int treeHeight) {
+                                       return treeHeight >= currentHeight;
+                                     })))),
+
+          .left = static_cast<int>(
+              column == 0 ? 0
+                          : std::abs(std::distance(
+                                treeMap[row].rend() - column - 1,
+                                std::find_if(treeMap[row].rbegin() +
+                                                 (treeMap[row].size() - column),
+                                             treeMap[row].rend() - 1,
+                                             [&](int treeHeight) {
+                                               return treeHeight >=
+                                                      currentHeight;
+                                             })))),
+          .right = static_cast<int>(
+              column == treeMap[0].size() - 1
+                  ? 0
+                  : std::abs(std::distance(treeMap[row].begin() + column,
+                                           std::find_if(treeMap[row].begin() +
+                                                            column + 1,
+                                                        treeMap[row].end() - 1,
+                                                        [&](int treeHeight) {
+                                                          return treeHeight >=
+                                                                 currentHeight;
+                                                        }))))};
+
+      scenicScoreMap[row][column] = scenicScore.up * scenicScore.down *
+                                    scenicScore.left * scenicScore.right;
+    }
+  }
+
+  return scenicScoreMap;
+}
+
 void puzzle8(std::vector<std::string> arguments) {
   FileReader::FileReader fr(arguments[0]);
 
@@ -81,25 +151,34 @@ void puzzle8(std::vector<std::string> arguments) {
   }
 
   auto heightMap = createMaxHeightMap(treeMap);
+  auto scenicScoreMap = createScenicScoreMap(treeMap);
 
   int visibleTrees = 0;
   for (int row = 0; row < treeMap.size(); row++) {
     for (int column = 0; column < treeMap[0].size(); column++) {
 
       int treeHeight = treeMap[row][column];
-      MaxHeightInEachDirection maxHeights = heightMap[row][column];
+      MaxValueInEachDirection maxHeights = heightMap[row][column];
 
-      std::cout << maxHeights.up << " " << maxHeights.down << " "
-                << maxHeights.left << " " << maxHeights.right << "\n";
       if (treeHeight > maxHeights.up || treeHeight > maxHeights.down ||
           treeHeight > maxHeights.left || treeHeight > maxHeights.right) {
-        std::cout << row << " " << column << ": Visible " << treeHeight << "\n";
         visibleTrees++;
       }
     }
   }
 
   std::cout << visibleTrees << "\n";
+
+  auto max = 0;
+  for (auto line : scenicScoreMap) {
+    for (auto scenicScore : line) {
+      if (scenicScore > max) {
+        max = scenicScore;
+      }
+    }
+  }
+
+  std::cout << max << "\n";
 }
 
 } // namespace Puzzles
